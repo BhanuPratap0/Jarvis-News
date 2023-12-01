bb047aae7cd75e9f61abab9fb365cc27320e8758
BhanuPratap0 <bhanu.pratap.singh.2020@ecajmer.ac.in>
2023-12-01 22:11:46 +0530

    s

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-2' >
         <div className="card" style={{width:'23rem'}} >
-          <span style={{left:'95%',zIndex:"1"}} class="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
+          <span style={{left:'95%',zIndex:"1"}} className="position-absolute top-0 translate-middle badge rounded-pill bg-danger">
           {source}
           </span>
         <img src={imageUrl}  className="card-img-top" alt="..."/>
@@ -14,7 +14,7 @@ export class NewsItem extends Component {
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}..</p>
             <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
-            <p class="card-text"><small class="text-muted">BY {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <p className="card-text"><small className="text-muted">BY {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
         </div>
         </div>
       </div>

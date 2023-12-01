c59ad6cdb95cdce61c6b4e0b9a18d070b47d2b60
BhanuPratap0 <bhanu.pratap.singh.2020@ecajmer.ac.in>
2023-12-01 22:07:03 +0530

    s

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import News from './News'
 import { Link } from 'react-router-dom'
 
 export class NavBar extends Component {
